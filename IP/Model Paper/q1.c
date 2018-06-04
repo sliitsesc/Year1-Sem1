df3f40e92e145c8fd120a6d848c32ea2703b595a
Chamod Shehanka <hcsperera@outlook.com>
2018-06-04 20:14:51 +0530

    Bug fixed
    Fixed char error

--- IP/Model Paper/q1.c
+++ IP/Model Paper/q1.c
@@ -1,23 +1,32 @@
-//Question 1
+//Question 1 
 #include <stdio.h>
+
 int main(void){
 
-    char roomType, payMethod, con;
+    //Variable declaration
+    char roomType, payMethod, con = 'N';
     int numOfRooms, night;
     double rate = 0, discount = 0, total;
 
+    printf("Room Type \t Description \t\t Rate per Night \n");
+    printf("  D \t\t Deluxe \t\t 31000.00 \n");
+    printf("  S \t\t Superior Deluxe \t 35000.00 \n");
+    printf("  C \t\t Club Suites \t\t 50000.00 \n");
+    printf("  E \t\t Executive Suites \t 75000.00 \n");
+    printf("  P \t\t Presidential Suite \t 100000.00 \n\n");
+
     do{
         printf("Input room type : ");
-        roomType = (char) getchar();
-
-        printf("Input payment method : ");
-        payMethod = (char) getchar();
+        scanf(" %c", &roomType);
 
         printf("Input number of rooms : ");
         scanf("%d", &numOfRooms);
 
-        printf("Input number of nights : ");
-        scanf("%d ", &night);
+        printf("Input no of nights : ");
+        scanf("%d", &night);
+
+        printf("Input Payment method : ");
+        scanf(" %c", &payMethod);
 
         switch (roomType){
             case 'D': rate = 31000;
@@ -30,21 +39,20 @@ int main(void){
                 break;
             case 'P': rate = 100000;
                 break;
-            default: printf("Invalid type ");
+            default: printf("Invalid type !!!");
         }
+
+        total = rate * night * numOfRooms;
 
         if (payMethod == 'C' || payMethod == 'c'){
-            discount = 0.1;
+            discount = total * 0.1;
         }
 
-        total = rate * numOfRooms * night;
-        total = rate + rate * discount;
+        total -= discount;
 
-        printf("Your total : %.2f \n", total);
+        printf("Your total amount : %.2f\n", total);
 
-        printf("Do you want to continue ?  ");
-        con = (char) getchar();
+        printf("Do you want to continue ? (Y/N)\n");
+        scanf(" %c", &con);
     }while (con == 'Y' || con == 'y');
-
-    return 0;
 }
